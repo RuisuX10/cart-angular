d5144d311f199995f98eeff4ee21f1c820668a8d
RuisuX10 <r.luisescobar@hotmail.com>
2025-02-10 15:40:16 -0300

    hasta clase 46, evento click y agregar al carro

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
+
+@Component({
+  selector: 'app-cart-app',
+  standalone: true,
+  imports: [CatalogComponent, CartComponent],
+  templateUrl: './cart-app.component.html'
+})
+export class CartAppComponent implements OnInit{
+
+  products: Product[] = [];
+  items: CartItem[] = [];
+
+  constructor(private service: ProductService) {}
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+  addToCart(product: Product) {
+    this.items = [...this.items, { product: {...product}, quantity: 1 }];
+  }
+}
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    { 
+        id: 1, 
+        name: 'Teclado mecanico Asus RGB', 
+        description: 'Algun teclado mecanico con luces RGB y switches cherry red', 
+        price: 100 
+    },
+    { 
+        id: 2, 
+        name: 'Samsung smart TV', 
+        description: 'Excelente TV OLED', 
+        price: 700 
+    },
+    { 
+        id: 3, 
+        name: 'Sony Camara video', 
+        description: 'camara para streaming', 
+        price: 80 
+    },
+    { 
+        id: 4, 
+        name: 'Corsair memoria ram 16 GB DDR5', 
+        description: 'ram optimizadas para videojuegos', 
+        price: 50 
+    },
+    { 
+        id: 5, 
+        name: 'RTX 4900', 
+        description: 'Tarjeta de video para juegos', 
+        price: 550 
+    },
+    { 
+        id: 6, 
+        name: 'CPU Intel core i7', 
+        description: 'optimizadas para tareas multicore', 
+        price: 400 
+    }
+  ];
