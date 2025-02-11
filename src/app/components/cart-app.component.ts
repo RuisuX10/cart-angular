3e4a884787cb6fb86a6a678f8f231d69825b9942
RuisuX10 <r.luisescobar@hotmail.com>
2025-02-11 15:39:33 -0300

    guardando datos en el commit storage, clase 50

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -20,6 +20,7 @@ export class CartAppComponent implements OnInit{
   constructor(private service: ProductService) {}
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
     this.calculateTotal();
   }
 
@@ -39,15 +40,20 @@ export class CartAppComponent implements OnInit{
       this.items = [...this.items, { product: {...product}, quantity: 1 }];
     }
     this.calculateTotal();
+    this.saveSession();
   }
 
   onDeleteCart(id: number) {
     this.items = this.items.filter(item => item.product.id !== id);
     this.calculateTotal();
+    this.saveSession();
   }
 
   calculateTotal(): void {
     this.total = this.items.reduce((acumulator, item) => acumulator + item.product.price * item.quantity, 0);
   }
 
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
+  }
 }
